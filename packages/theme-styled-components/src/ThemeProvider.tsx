283170d05f49b46f4a8caccffd0bc378b8ef86cc
snowjang24 <asdka4618@gmail.com>
2021-07-18 21:59:32 +0900

    .

---
+++ packages/theme-styled-components/src/ThemeProvider.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import {
+  ThemeProvider as StyledComponentsThemeProvider,
+  createGlobalStyle,
+} from "styled-components";
+
+import {color} from '@denations/design-token'
